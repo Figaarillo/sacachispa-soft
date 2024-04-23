30f1a152653ccc24e5c94a7736ec24fca176c5c0
Figaarillo <axel.leonardi.22@gmail.com>
2024-04-23 12:20:50 -0300

    test: add test for brand creation

---
+++ backend/internal/entity/brand_test.go
@@ -0,0 +1,68 @@
+package entity_test
+
+import (
+	"testing"
+
+	"github.com/Figaarillo/sacachispa-soft/internal/entity"
+)
+
+func TestBrand(t *testing.T) {
+	t.Run("Test NewBrand() with valid fields", func(t *testing.T) {
+		brand, err := entity.NewBrand("brand", "brand description")
+		if err != nil {
+			t.Fatalf("Failed to create brand: %v", err)
+		}
+
+		if brand == nil {
+			t.Fatal("Failed to create brand: brand is nil")
+		}
+
+		if brand.Name != "brand" {
+			t.Errorf("Expected brand name 'brand', got %s", brand.Name)
+		}
+
+		if brand.Description != "brand description" {
+			t.Errorf("Expected brand description 'brand description', got %s", brand.Description)
+		}
+	})
+
+	t.Run("Test NewBrand() with invalid fields", func(t *testing.T) {
+		_, err := entity.NewBrand("", "")
+		if err == nil {
+			t.Error("Expected error for empty name, got nil")
+		}
+	})
+
+	t.Run("Test NewBrand() without fields", func(t *testing.T) {
+		_, err := entity.NewBrand()
+		if err == nil {
+			t.Error("Expected error for empty name, got nil")
+		}
+	})
+
+	t.Run("Test Update() with valid fields", func(t *testing.T) {
+		brand, err := entity.NewBrand("brand")
+		if err != nil {
+			t.Fatalf("Failed to create brand: %v", err)
+		}
+
+		if err := brand.Update("brand2"); err != nil {
+			t.Errorf("Failed to update brand: %v", err)
+		}
+
+		if brand.Name != "brand2" {
+			t.Errorf("Expected brand name 'brand2', got %s", brand.Name)
+		}
+	})
+
+	t.Run("Test Update() with invalid fields", func(t *testing.T) {
+		brand, err := entity.NewBrand("brand")
+		if err != nil {
+			t.Fatalf("Failed to create brand: %v", err)
+		}
+
+		if err := brand.Update(""); err == nil {
+			t.Error("Expected error for empty name, got nil")
+		}
+	})
+}
