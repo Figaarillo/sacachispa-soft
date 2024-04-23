249056a702f0825edd9b3dbcaf4872a0f7db448e
Figaarillo <axel.leonardi.22@gmail.com>
2024-04-23 12:52:37 -0300

    test: create brand entity

--- backend/internal/entity/brand.go
+++ backend/internal/entity/brand.go
@@ -1,1 +1,43 @@
 package entity
+
+import "time"
+
+type Brand struct {
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	ID          ID        `json:"id"`
+}
+
+func NewBrand(payload Brand) (*Brand, error) {
+	brand := &Brand{
+		ID:          NewID(),
+		Name:        payload.Name,
+		Description: payload.Description,
+	}
+
+	if err := brand.Validate(); err != nil {
+		return nil, err
+	}
+
+	return brand, nil
+}
+
+func (b *Brand) Update(payload Brand) error {
+	b.Name = payload.Name
+	b.Description = payload.Description
+
+	if err := b.Validate(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *Brand) Validate() error {
+	if b.Name == "" || b.Description == "" {
+		return ErrMissingField
+	}
+	return nil
+}
---
+++ backend/internal/entity/entity_errors.go
@@ -0,0 +1,5 @@
+package entity
+
+import "errors"
+
+var ErrMissingField = errors.New("missing field")
