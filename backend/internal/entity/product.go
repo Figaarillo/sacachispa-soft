e2d250145d739db908b9e878ae837fad58e16d0d
Figaarillo <axel.leonardi.22@gmail.com>
2024-04-23 22:20:41 -0300

    feat: create product entity

---
+++ backend/internal/entity/product.go
@@ -0,0 +1,63 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/Figaarillo/sacachispa-soft/internal/util"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+	Name        string         `json:"name" gorm:"unique;not null"`
+	Description string         `json:"description"`
+	Brand       Brand          `json:"brand" gorm:"foreignKey:BrandID"`
+	Stock       int            `json:"stock"`
+	Price       float64        `json:"price"`
+	BrandID     ID             `json:"brand_id" gorm:"not null"`
+	ID          ID             `json:"id"`
+}
+
+func NewProduct(payload Product) (*Product, error) {
+	product := &Product{
+		ID:          NewID(),
+		Name:        payload.Name,
+		Description: payload.Description,
+		Stock:       payload.Stock,
+		Price:       payload.Price,
+		BrandID:     payload.BrandID,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	if err := product.Validate(); err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *Product) Update(payload Product) error {
+	util.AssignIfNotEmpty(&p.Name, payload.Name)
+	util.AssignIfNotEmpty(&p.Description, payload.Description)
+	util.AssignIfNotZero(&p.Stock, payload.Stock)
+	util.AssignIfNotZeroFloat(&p.Price, payload.Price)
+	util.AssignUUIDIFNotEmpty(&p.BrandID, payload.BrandID)
+	p.UpdatedAt = time.Now()
+
+	if err := p.Validate(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Product) Validate() error {
+	if p.Name == "" || p.Description == "" || p.BrandID.String() == "" || p.Stock == 0 || p.Price == 0 {
+		return ErrMissingField
+	}
+
+	return nil
+}
--- backend/internal/util/field_assign_helper.go
+++ backend/internal/util/field_assign_helper.go
@@ -1,7 +1,27 @@
 package util
 
+import "github.com/google/uuid"
+
 func AssignIfNotEmpty(field *string, newValue string) {
 	if newValue != "" {
 		*field = newValue
+	}
+}
+
+func AssignIfNotZero(field *int, newValue int) {
+	if newValue != 0 {
+		*field = newValue
+	}
+}
+
+func AssignIfNotZeroFloat(field *float64, newValue float64) {
+	if newValue != 0 {
+		*field = newValue
+	}
+}
+
+func AssignUUIDIFNotEmpty(id *uuid.UUID, newID uuid.UUID) {
+	if newID.String() != "" {
+		*id = newID
 	}
 }
